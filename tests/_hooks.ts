d1473d4e26e9d489c08f983554598df185b4663f
Shalior <ali_shalior@proton.me>
2023-01-06 19:06:01 +0330

    update test hooks functions

--- tests/_hooks.ts
+++ tests/_hooks.ts
@@ -1,26 +1,19 @@
-import {start, stop} from '../src/lifecycle';
+import { start, stop } from '../src/lifecycle';
 import db from "../src/db";
 
 type DoneFunction = () => {};
 
 export const mochaHooks = {
 	beforeAll(done: DoneFunction) {
-		start({queues: false, server: true})
-			.then(() => {
-				db.migrate.latest().then(() => {
-					done()
-				})
-			})
+		start({ queues: false, server: true })
+			.then(() => done())
 			.catch(err => console.log(err));
 	},
 
 	afterAll(done: DoneFunction) {
-		db.migrate.rollback(undefined, true).then(() => {
-			stop()
-				.then(() => {
-					done();
-				}).catch(err => console.log(err));
-		}).catch(err => console.log(err));
+		stop()
+			.then(() => done())
+			.catch(err => console.log(err));
 
 	},
 };
